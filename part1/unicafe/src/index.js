f3b373a13faf9b38df5ed0a35fa8832e081286ed
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-07-03 16:38:43 +0300

    unicafe step2, 3, 4 complete

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,19 +1,76 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
   // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setTotal] = useState(0);
 
+  const handleClickForNeutral = () => {
+    setNeutral(neutral + 1);
+    setTotal(all + 1);
+  };
+  const handleClickForBad = () => {
+    setBad(bad + 1);
+    setTotal(all + 1);
+  };
+  const handleClickForGood = () => {
+    setGood(good + 1);
+    setTotal(all + 1);
+  };
   return (
     <div>
-      code here
+    <h3>give feedback</h3>
+      <Feedback
+        handleClickForNeutral={handleClickForNeutral}
+        handleClickForBad={handleClickForBad}
+        handleClickForGood={handleClickForGood}
+      />
+      <h3>statistics</h3>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={good + neutral + bad}
+        average={(good + neutral + bad) / 3}
+        positive={(good / all) * 100}
+      />
+    </div>
+  );
+};
+const Feedback = ({
+  handleClickForGood,
+  handleClickForBad,
+  handleClickForNeutral,
+}) => {
+  return (
+    <div>
+      <button onClick={handleClickForGood}>good</button>
+      <button onClick={handleClickForNeutral}>neutral</button>
+      <button onClick={handleClickForBad}>bad</button>
+    </div>
+  );
+};
+
+const Statistics = (props) => {
+  if (props.all > 0) {
+    return (
+      <div>
+        <p>good {props.good}</p>
+        <p>neutral {props.neutral}</p>
+        <p>bad {props.bad}</p>
+        <p>all {props.all}</p>
+        <p>average {props.average}</p>
+        <p>positive {props.positive}%</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+   <p>No feedback given</p> 
     </div>
   )
-}
-
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+};
+ReactDOM.render(<App />, document.getElementById("root"));
