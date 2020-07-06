b7a4e2dbffe5413602b4f69135da242e82907cae
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-07-06 12:10:44 +0300

    anecdotes step2 complete

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const App = (props) => {
+ 
+  const [votes, setVotes] = useState(0);
   const [selected, setSelected] = useState(0);
-  const handleClick = () => {
-    const random = Math.floor(Math.random() * 5);
+  useEffect(() => {
+    setVotes(props.copy[0]);
+  }, [props.copy]);
+  const handleNext = (e) => {
+    e.preventDefault();
+    const random = Math.floor(Math.random() * 6);
     setSelected(random);
+    setVotes(props.copy[random]);
   };
+  const handleVote = (e) => {
+    e.preventDefault();
+
+    props.copy[selected] += 1;
+    setVotes(props.copy[selected]);
+    console.log("points array is ", props.copy);
+    console.log("selected index is ", selected);
+  };
+
   return (
     <div>
       <p>{props.anecdotes[selected]}</p>
-      <button onClick={handleClick}>next anecdotes</button>
+      <p>has {votes} votes</p>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNext}>next anecdotes</button>
     </div>
   );
 };
@@ -23,5 +41,10 @@ const anecdotes = [
   "Premature optimization is the root of all evil.",
   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
 ];
+const points = new Array(2, 5, 7, 9, 1, 4); //.fill(0);
+const copy = [...points];
 
-ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
+ReactDOM.render(
+  <App anecdotes={anecdotes} copy={copy} />,
+  document.getElementById("root")
+);
