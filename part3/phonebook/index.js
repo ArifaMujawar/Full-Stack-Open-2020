903fc6d20548d9a992470df81bbcfd132c78b758
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-07-22 21:40:35 +0300

    Exercise 3.13 and 3.14 complete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,47 +1,60 @@
 const express = require("express");
+require('dotenv').config()
+const Person = require('./models/persons')
+
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors')
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+
+// const Person = mongoose.model("Person", personSchema);
+
 
 app.use(express.json());
-app.use(express.static('build'))
-app.use(cors())
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-  
-];
+app.use(express.static("build"));
+app.use(cors());
+// let persons = [
+//   {
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//     id: 1,
+//   },
+//   {
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//     id: 2,
+//   },
+//   {
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//     id: 3,
+//   },
+//   {
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//     id: 4,
+//   },
+
+// ];
 //app.use(morgan('tiny'));
 
-morgan.token('host', function(req, res) {
- 
+morgan.token("host", function (req, res) {
   return JSON.stringify(req.body);
 });
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :host'));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :host")
+);
 app.get("/", (req, res) => {
   res.send("<h1>Hello ohh world");
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  console.log('In api/persons');
+  Person.find({}).then((p) => {
+    res.json(p);
+    console.log('p value ',p);
+  });
 });
 
 app.get("/info", (req, res) => {
@@ -65,27 +78,33 @@ app.delete("/api/persons/:id", (req, res) => {
 });
 
 app.post("/api/persons", (req, res) => {
-  const random = Math.floor(Math.random() * 100);
+  // const random = Math.floor(Math.random() * 100);
   const body = req.body;
 
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: "name or number missing",
     });
-  } else if (
-    persons.some((p) => p.name.toLowerCase() === body.name.toLowerCase())
-  ) {
-    return res.status(400).json({
-      error: "name already exists",
-    });
-  }
-  const data = {
+   }
+   //else if (
+  //   persons.some((p) => p.name.toLowerCase() === body.name.toLowerCase())
+  // ) {
+  //   return res.status(400).json({
+  //     error: "name already exists",
+  //   });
+  // }
+  const data = new Person({
     name: body.name,
-    number: body.number,
-    id: random,
-  };
-  persons = persons.concat(data);
-  res.json(data);
+    phoneNumber: body.number,
+    // id: random,
+  });
+  // persons = persons.concat(data);
+  data.save().then(savedPerson=>{
+    res.json(savedPerson);
+  }).catch((e)=>{
+    console.log('error ', e);
+  })
+  
 });
 
 const PORT = process.env.PORT || 3001;
