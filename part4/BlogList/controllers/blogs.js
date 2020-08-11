a345f81bbac113086733e6dd6f9120151dfb90c5
Arifa <arifabegam.mujawar@gmail.com>
2020-08-11 13:15:22 +0300

    Part 4b completed

--- part4/BlogList/controllers/blogs.js
+++ part4/BlogList/controllers/blogs.js
@@ -11,19 +11,54 @@ blogRouter.get('/', (request, response) => {
     })
 })
 
+
+blogRouter.get('/:id', async (request, response, next) => {
+  console.log('within :id')
+  await Blog.findById(request.params.id, (err, data) => {
+    console.log('data is ', data)
+    if (!data) {
+      response.status(404).end()
+
+    } else {
+      response.json(data.toJSON())
+    }
+  }).catch(e => next(e))
+
+
+})
+
 blogRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
   if (!blog.hasOwnProperty('likes')) {
     blog['likes'] = 0;
   }
-  if(!request.body.hasOwnProperty('title') || !request.body.hasOwnProperty('url')){
+  if (!request.body.hasOwnProperty('title') || !request.body.hasOwnProperty('url')) {
     console.log('missing props');
-    response.status(400).json({ error: 'content missing' })
+    return response.status(400).json({ error: 'content missing' })
   }
   blog
     .save()
     .then(result => {
       response.status(201).json(result)
     })
+})
+
+blogRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndRemove(req.params.id)
+  res.status(204).end()
+})
+
+blogRouter.put('/:id', (req, res, next) => {
+
+  const body = req.body
+  console.log('body ', body)
+  const blog = {
+    likes: body.likes
+  }
+
+  console.log('blog ', blog)
+  Blog.findByIdAndUpdate(req.params.id, blog, { new: true }).then(updatedBlog => {
+    res.json(updatedBlog.toJSON())
+  }).catch(e => next(e))
 })
 module.exports = blogRouter
--- part4/BlogList/tests/blog_api.test.js
+++ part4/BlogList/tests/blog_api.test.js
@@ -21,59 +21,118 @@ const initialBlog = [
 beforeEach(async () => {
   await Blog.deleteMany({});
 
-  let noteObject = new Blog(initialBlog[0]);
-  await noteObject.save();
+  // let noteObject = new Blog(initialBlog[0]);
+  // await noteObject.save();
 });
 
-test("blogs are returned as json", async () => {
-  await api.get("/api/blogs").expect("Content-Type", /application\/json/);
+describe("when there is initially some blogs saved", () => {
+  test("blogs are returned as json", async () => {
+    await api.get("/api/blogs").expect("Content-Type", /application\/json/);
+  });
+
+  test("all the blogs are returned", async () => {
+    await api.post("/api/blogs").send(initialBlog[0]).expect(201);
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(1);
+  });
+  test("a specific title is within the returned blog", async () => {
+    await api.post("/api/blogs").send(initialBlog[1]).expect(201);
+    const response = await api.get('/api/blogs')
+
+   
+
+    const titles = response.body.map(r => r.title)
+    console.log('title ', titles)
+    expect(titles).toContain('Arifa in my title')
+  });
 });
 
-test("verify unique identifier property", async () => {
-  await api.post("/api/blogs").send(initialBlog[0]).expect(201);
 
-  const response = await api.get("/api/blogs");
-
-  expect(response.body[0]).toEqual(
-    expect.objectContaining({
-      id: expect.any(String),
-    })
-  );
-
-  expect(response.body[0].id).toBeDefined();
-});
-
-test("verify HTTP POST request", async () => {
-  await Blog.deleteMany({});
-  await api.post("/api/blogs").send(initialBlog[0]).expect(201);
-
-  const res = await api.get("/api/blogs");
-  expect(res.body).toHaveLength(1);
-});
-
-test("verify if likes property is missing", async () => {
-  await Blog.deleteMany({});
-  const data = {
-    title: "Arifa in my title",
-    author: "astr",
-    url: "http://google.com",
-  };
+describe('Viewing a specific blog', ()=>{
+  test("verify unique identifier property is named as id", async () => {
+    await api.post("/api/blogs").send(initialBlog[0]).expect(201);
   
-  await api.post("/api/blogs").send(data).expect(201);
-  const res = await api.get("/api/blogs");
-  console.log(res.body);
-  expect(res.body).toHaveLength(1);
-});
-
-test('url and title is missing', async () => {
+    const response = await api.get("/api/blogs");
   
-  const data = {
-    author:'sheep',
-    like:9
-  }
+    expect(response.body[0]).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+      })
+    );
+  
+    expect(response.body[0].id).toBeDefined();
+  });
 
-  await api.post('/api/blogs').send(data).expect(400)
+  test('fails with statuscode 404 id is invalid',async()=>{
+    const invalidId = '234n2458kvs83n'
+
+    await api.get(`/api/blogs/${invalidId}`).expect(404)
+  })
+
+  
 })
+
+describe('addition of new blog', async()=>{
+  test("verify HTTP POST request", async () => {
+    await Blog.deleteMany({});
+    await api.post("/api/blogs").send(initialBlog[0]).expect(201);
+  
+    const res = await api.get("/api/blogs");
+    expect(res.body).toHaveLength(1);
+  });
+
+  test("url and title is missing", async () => {
+    const data = {
+      author: "sheep",
+      like: 9,
+    };
+  
+    await api.post("/api/blogs").send(data).expect(400);
+  });
+
+
+  test("verify if likes property is missing", async () => {
+    await Blog.deleteMany({});
+    const data = {
+      title: "Arifa in my title",
+      author: "astr",
+      url: "http://google.com",
+    };
+  
+    await api.post("/api/blogs").send(data).expect(201);
+    const res = await api.get("/api/blogs");
+    console.log(res.body);
+    expect(res.body).toHaveLength(1);
+  });
+})
+
+
+describe('deletion of a blog', async ()=>{
+  test('succeeds with status code 204 if id is valid', async()=>{
+    await api.post("/api/blogs").send(initialBlog[1]).expect(201);
+    const blogs = await Blog.find({})
+    console.log('check here-------- ', blogs)
+    const BlogsAtStart = await blogs.map(blog => blog.toJSON())
+    console.log('BlogsAtStart ', BlogsAtStart)
+    const blogToDelete = BlogsAtStart[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogs1 = await Blog.find({})
+    const BlogsAtEnd = await blogs1.map(blog => blog.toJSON())
+    
+
+    expect(BlogsAtEnd).toHaveLength(0)
+
+    const contents = BlogsAtEnd.map(r => r.author)
+
+    expect(contents).not.toContain(blogToDelete.author)
+  })
+})
+
+
+
+
 afterAll(() => {
   mongoose.connection.close();
 });
